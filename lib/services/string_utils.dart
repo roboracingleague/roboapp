bedfbad32d9cac7947072d12c8ab557c6080302c
Muldec <34677746+Muldec@users.noreply.github.com>
2020-02-27 10:54:36 +0100

    Remove unnecessary code

--- lib/services/string_utils.dart
+++
@@ -1,6 +0,0 @@
-/// Provides Strings utility methods
-class StringUtils {
-  /// Returns the string of an enum value
-  static String enumName(Object enumToString) =>
-      enumToString.toString().split('.').last;
-}
